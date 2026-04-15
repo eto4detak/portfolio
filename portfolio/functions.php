5fdd5e5ec70a9ad488415555c1c038b368f564ee
eto4detak <niclokitor@gmail.com>
2026-04-15 17:29:39 +0300

    delete footer type

--- portfolio/functions.php
+++ portfolio/functions.php
@@ -157,51 +157,6 @@ function eto_register_post_types_slider(){
 
 }
 
-add_action( 'init', 'eto_register_post_types_footer' );
-
-function eto_register_post_types_footer(){
-
-	register_post_type( 'footer', [
-		'label'  => null,
-		'labels' => [
-			'name'               => 'Footer', // основное название для типа записи
-			'singular_name'      => 'footer', // название для одной записи этого типа
-			'add_new'            => 'Добавить footer', // для добавления новой записи
-			'add_new_item'       => 'Добавление footer', // заголовка у вновь создаваемой записи в админ-панели.
-			'edit_item'          => 'Редактирование footer', // для редактирования типа записи
-			'new_item'           => 'Новое footer', // текст новой записи
-			'view_item'          => 'Смотреть footer', // для просмотра записи этого типа.
-			'search_items'       => 'Искать footer', // для поиска по этим типам записи
-			'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
-			'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
-			'parent_item_colon'  => '', // для родителей (у древовидных типов)
-			'menu_name'          => 'Footer', // название меню
-		],
-		'description'            => '',
-		'public'                 => true,
-		// 'publicly_queryable'  => null, // зависит от public
-		// 'exclude_from_search' => null, // зависит от public
-		// 'show_ui'             => null, // зависит от public
-		// 'show_in_nav_menus'   => null, // зависит от public
-		'show_in_menu'           => null, // показывать ли в меню админки
-		// 'show_in_admin_bar'   => null, // зависит от show_in_menu
-		'show_in_rest'        => null, // добавить в REST API. C WP 4.7
-		'rest_base'           => null, // $post_type. C WP 4.7
-		'menu_position'       => null,
-		'menu_icon'           => null,
-		//'capability_type'   => 'post',
-		//'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
-		//'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
-		'hierarchical'        => false,
-		'supports'            => [ 'title', 'editor', 'thumbnail', ], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
-		'taxonomies'          => [],
-		'has_archive'         => false,
-		'rewrite'             => true,
-		'query_var'           => true,
-	] );
-
-}
-
 
 //# изменить класс в коменте
 add_filter( 'comment_reply_link', 'eto_comment_reply_link_class' );
